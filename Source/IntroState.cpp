6b2507fc21ceadfa8a1f7346942af62b42076477
Alexander "Ace" Olofsson <ace@haxalot.com>
2014-04-26 02:17:52 +0200

    Removed debugging data from the Intro state

--- Source/IntroState.cpp
+++ Source/IntroState.cpp
@@ -2,39 +2,9 @@
 #include "MenuState.hpp"
 #include "Util/FontFinder.hpp"
 
-#include "Util/QuadTree.hpp"
-
-#include "Game/Actor.hpp"
-
 #include <SFML/Window/Mouse.hpp>
 #include <SFML/Graphics/RenderTarget.hpp>
 #include <SFML/Graphics/Text.hpp>
-
-#include <SFML/Graphics/RenderWindow.hpp>
-
-QuadTree tree(sf::FloatRect(0, 0, 1024, 768), 5);
-
-class MouseActor : public Actor
-{
-public:
-	MouseActor(sf::RenderWindow& win) : mWin(win) { }
-
-	void update()
-	{
-		mPos = (sf::Vector2f)sf::Mouse::getPosition(mWin);
-	}
-
-	sf::Vector2f getPosition() const
-	{
-		return mPos;
-	}
-
-private:
-	sf::Vector2f mPos;
-	sf::RenderWindow& mWin;
-};
-
-MouseActor* temp = nullptr;
 
 IntroState::IntroState() : mTime(0)
 {
@@ -48,11 +18,7 @@ IntroState::~IntroState()
 
 void IntroState::update(double dt)
 {
-	if (temp)
-		temp->update();
 	mTime += dt;
-
-	tree.update(dt);
 
 	if (mTime > 1)
 	{
@@ -64,14 +30,6 @@ void IntroState::update(double dt)
 
 void IntroState::draw(sf::RenderTarget& target)
 {
-	if (!temp)
-	{
-		temp = new MouseActor((sf::RenderWindow&)target);
-		tree.addActor(temp);
-	}
-
-	tree.draw(target);
-
 	sf::Text introText("<Intro placeholder>", mFont);
 	{
 		auto rect = introText.getLocalBounds();
