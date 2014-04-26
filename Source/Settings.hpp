c9a694201d2abdcb9f4b7fc67ced1427dd3eeadd
Alexander "Ace" Olofsson <ace@haxalot.com>
2014-04-26 12:29:53 +0200

    Fix Visual Studio non-compliance issue

--- Source/Settings.hpp
+++ Source/Settings.hpp
@@ -2,6 +2,8 @@
 
 #include "Util/INI.hpp"
 #include <string>
+#include <stdexcept>
+#include <type_traits>
 
 const std::string CONFIG_PATH = "settings.ini";
 
@@ -13,26 +15,39 @@ public:
 
 	static bool hasSetting(const std::string& section, const std::string& key);
 	template<typename T>
-	static T getSetting(const std::string& section, const std::string& key);
-	template<>
-	static std::string getSetting<std::string>(const std::string& section, const std::string& key);
+	static T getSetting(const std::string& section, const std::string& key)
+	{
+		return privGetSetting<T>(section, key, std::is_same<T, std::string>::type());
+	}
 	template<typename T>
-	static void setSetting(const std::string& section, const std::string& key, T value);
-	template<>
-	static void setSetting<std::string>(const std::string& section, const std::string& key, std::string value);
+	static void setSetting(const std::string& section, const std::string& key, T value)
+	{
+		privSetSetting(section, key, value);
+	}
 
 private:
+	template<typename T>
+	struct identity { typedef T type; };
+
+	static void privSetSetting(const std::string& section, const std::string& key, std::string value) { sFile.addSetting(section, key, value); }
+	template<typename T>
+	static void privSetSetting(const std::string& section, const std::string& key, T value);
+	template<typename T>
+	static std::string privGetSetting(const std::string& section, const std::string& key, const std::true_type&);
+	template<typename T>
+	static T privGetSetting(const std::string& section, const std::string& key, const std::false_type&);
+
 	static INIFile sFile;
 };
 
-template<>
-std::string Settings::getSetting(const std::string& section, const std::string& key)
+template<typename T>
+std::string Settings::privGetSetting(const std::string& section, const std::string& key, const std::true_type&)
 {
 	return sFile.getValue(section, key);
 }
 
 template<typename T>
-T Settings::getSetting(const std::string& section, const std::string& key)
+T Settings::privGetSetting(const std::string& section, const std::string& key, const std::false_type&)
 {
 	std::string data = sFile.getValue(section, key);
 	if (data.empty())
@@ -43,15 +58,8 @@ T Settings::getSetting(const std::string& section, const std::string& key)
 
 	return value;
 }
-
-template<>
-void Settings::setSetting<std::string>(const std::string& section, const std::string& key, std::string value)
-{
-	sFile.addSetting(section, key, value);
-}
-
 template<typename T>
-void Settings::setSetting(const std::string& section, const std::string& key, T value)
+void Settings::privSetSetting(const std::string& section, const std::string& key, T value)
 {
 	char tmp[256];
 	sprintf(tmp, (std::string("%") + key[0]).c_str(), value);
