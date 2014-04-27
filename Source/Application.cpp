31fee8c089ffd3513cb287efb3217f291dbe5926
Alexander "Ace" Olofsson <ace@haxalot.com>
2014-04-28 00:15:40 +0200

    Stop double precision from having trouble

--- Source/Application.cpp
+++ Source/Application.cpp
@@ -90,7 +90,7 @@ int Application::run()
 
 		mWindow.display();
 
-		std::this_thread::sleep_for(std::chrono::microseconds(500));
+		std::this_thread::sleep_for(std::chrono::milliseconds(2));
 	}
 
 	return 0;
